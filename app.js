edd7aabefe7ad82ef180fa230e5c22d792366d8e
Arkan Tandel <arkantandel@gmail.com>
2025-12-24 06:11:02 +0000

    this is the code that I have changed

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import Express framework
+var express = require('express');
+
+// Create Express application
+var app = express();
+
+// Respond for root URL "/"
+app.get('/', function (req, res) {
+  res.send('Welcome to Node-app');
+});
+
+// Listen on port (3000 locally or env port in cloud)
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server is running on port 3000');
+});
+
+// Export app for external use
+module.exports = app;
